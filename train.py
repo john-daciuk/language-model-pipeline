81edc44c6ec7c7460b68f7a4856d0fbe998711aa
john daciuk <jdaciuk@roblox.com>
2021-02-19 14:16:43 -0800

    little stuff

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ def build_model(maxlen, chars):
     return model
 
 def main():
-    epochs = 40
+    epochs = 20
     batch_size = 128
     input_path = os.getenv('VH_INPUTS_DIR')
     data_path = os.path.join(input_path, 'data/data')
@@ -40,35 +40,33 @@ def main():
         x, y = f['x'], f['y']
     
     model = build_model(maxlen, chars)
+    model.fit(x, y, batch_size=batch_size, epochs=epochs)
+    print()
+    print("Generating text after training for {} epochs:  ".format(epochs))
 
-    for epoch in range(epochs):
-        model.fit(x, y, batch_size=batch_size, epochs=1)
+    start_index = random.randint(0, len(text) - maxlen - 1)
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        print("...Diversity:", diversity)
+
+        generated = ""
+        sentence = text[start_index : start_index + maxlen]
+        print('...Generating with seed: "' + sentence + '"')
+
+        for i in range(400):
+            x_pred = np.zeros((1, maxlen, len(chars)))
+            for t, char in enumerate(sentence):
+                x_pred[0, t, char_indices[char]] = 1.0
+            preds = model.predict(x_pred, verbose=0)[0]
+            next_index = sample(preds, diversity)
+            next_char = indices_char[next_index]
+            sentence = sentence[1:] + next_char
+            generated += next_char
+
+        print("...Generated: ", generated)
         print()
-        print("Generating text after epoch: %d" % epoch)
 
-        start_index = random.randint(0, len(text) - maxlen - 1)
-        for diversity in [0.2, 0.5, 1.0, 1.2]:
-            print("...Diversity:", diversity)
-
-            generated = ""
-            sentence = text[start_index : start_index + maxlen]
-            print('...Generating with seed: "' + sentence + '"')
-
-            for i in range(400):
-                x_pred = np.zeros((1, maxlen, len(chars)))
-                for t, char in enumerate(sentence):
-                    x_pred[0, t, char_indices[char]] = 1.0
-                preds = model.predict(x_pred, verbose=0)[0]
-                next_index = sample(preds, diversity)
-                next_char = indices_char[next_index]
-                sentence = sentence[1:] + next_char
-                generated += next_char
-
-            print("...Generated: ", generated)
-            print()
-
-            OUTPUTS_DIR = os.getenv('VH_OUTPUTS_DIR', './outputs')
-            model.save(os.path.join(OUTPUTS_DIR, 'model.h5'), model)
+    OUTPUTS_DIR = os.getenv('VH_OUTPUTS_DIR', './outputs')
+    model.save(os.path.join(OUTPUTS_DIR, 'model.h5'), model)
 
     
         
